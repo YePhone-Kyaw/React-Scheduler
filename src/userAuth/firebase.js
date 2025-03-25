b6ff3c64c024e0b83b752fd78541d78a43bcbdd9
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-03-24 18:32:27 -0600

    tiny changes

--- src/userAuth/firebase.js
+++ src/userAuth/firebase.js
@@ -5,13 +5,12 @@ import { getStorage } from 'firebase/storage';
 import { GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCey0QGSAn2QsMyKrHJQ5nojR92KOXMiE8",
-  authDomain: "powershift-29522.firebaseapp.com",
-  projectId: "powershift-29522",
-  storageBucket: "powershift-29522.appspot.com",
-  messagingSenderId: "880153414164",
-  appId: "1:880153414164:web:402906cbf5ba9fde673501",
-  measurementId: "G-KV9E3CL7BF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
